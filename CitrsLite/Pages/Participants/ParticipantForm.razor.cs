c9e1336d1de4cdcdce77215962facccc77e9afe0
Don Vu <Don.Vu@fdacs.gov>
2023-01-27 09:26:22 -0500

    restructuring of participant index, updated participant detail view

---
+++ CitrsLite/Pages/Participants/ParticipantForm.razor.cs
@@ -0,0 +1,50 @@
+﻿using CitrsLite.Business.ViewModels.ParticipantViewModels;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+
+namespace CitrsLite.Pages.Participants
+{
+    public partial class ParticipantForm
+    {
+        [Inject]
+        public ParticipantFormViewModel Model { get; set; }
+        private EditContext EC { get; set; }
+        private bool success;
+
+        public void CreateParticipant(EditContext context)
+        {
+            if (EC.Validate() == true)
+            {
+                participantService.CreateAsync(Model);
+                success = true;
+                StateHasChanged();
+                ClearForm();
+            }
+            else
+            {
+                success = false;
+            }
+
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            setUserName();
+            EC = new EditContext(Model);
+            base.OnInitialized();
+        }
+
+        private void ClearForm()
+        {
+            Model = new ParticipantFormViewModel();
+            setUserName();
+            EC = new EditContext(Model);
+        }
+
+        private async void setUserName()
+        {
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            Model.UserName = authState.User.Identity?.Name ?? "Unknown user";
+        }
+    }
+}
